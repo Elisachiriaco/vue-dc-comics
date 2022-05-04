34c46739ede3a8f7bd8c22fb1cfb79ede1414e2f
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-05-04 17:35:06 +0200

    main completato

--- src/components/AppGrid.vue
+++ src/components/AppGrid.vue
@@ -1,15 +1,91 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="content">
+            <h1>Content Goes Here</h1>
+        </div>
+
+        <div class="image-container">
+            <ul v-for="(image, index) in images" :key="index">
+                <li><img :src="image.img" :alt="image.testo"></li>
+                <li>{{image.testo}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'AppGrid'
+    name:'AppGrid',
+    data(){
+        return{
+            images: [
+                {
+                    testo:'digital comics',
+                    img: require("../assets/images/buy-comics-digital-comics.png"),
+                    link: '',
+                    active: true,
+                },
+                {
+                    testo:'dc merchandise',
+                    img: require("../assets/images/buy-comics-merchandise.png"),
+                    link: '',
+                    active: true,
+                },
+                {
+                    testo:'subscription',
+                    img: require("../assets/images/buy-comics-subscriptions.png"),
+                    link: '',
+                    active: true,
+                },
+                {
+                    testo:'comic shop locator',
+                    img: require("../assets/images/buy-comics-shop-locator.png"),
+                    link: '',
+                    active: true,
+                },
+                {
+                    testo:'dc power visa',
+                    img: require("../assets/images/buy-dc-power-visa.svg"),
+                    link: '',
+                    active: true,
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+@import '../style/variables';
 
+.content{
+    background-color: $bgblack;
+    padding: 10px;
+
+    h1{
+    color: $bgwhite;
+    }
+}
+
+.image-container{
+    background-color: $bgazzurro;
+    display:flex;
+    justify-content: space-around;
+
+    ul{
+        list-style-type: none;
+        padding: 20px;
+        text-transform: uppercase;
+        color: $bgwhite;
+        align-items: center;
+        display: flex;
+    }
+    li{
+        padding: 10px;
+    }
+    img{
+        width:30px;
+        height:40px;
+    }
+}
 </style>
